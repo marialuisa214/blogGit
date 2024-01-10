1851a9f2fcb18b6c14cada552c3c9f1f565da99f
Maria Luisa <maria.luisa.alves214@gmail.com>
2024-01-10 15:40:50 -0300

    :nail_care: feat: pagina Post

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,5 +1,52 @@
+import { ArrowSquareOut, Calendar, CaretLeft, ChatCircle, GithubLogo } from "@phosphor-icons/react";
+import { useTheme } from "styled-components";
+import { BoxInfoPost, ButtonContainer, ContainerPost, ContentPost, ExtraInformations } from "./styles";
+
 export function Post(){
+    const theme = useTheme();
     return(
-        <h1>Post</h1>
+        <ContainerPost>
+            <BoxInfoPost>
+
+            <ButtonContainer>
+                <button>
+                    <CaretLeft size={16} color={theme['blue']} />
+                    VOLTAR
+                </button>
+
+                <button>
+                    <span>VER NO GITHUB</span>
+                    <ArrowSquareOut size={17} color={theme['blue']} />
+                </button>
+            </ButtonContainer>
+
+            <h1>JavaScript data types and data structures</h1>
+
+            <ExtraInformations>
+                <div>
+                    <GithubLogo size={20} color={theme['blue']} weight="fill" />
+                    <label>Repositórios</label>
+                </div>
+
+                <div>
+                    <Calendar size={20} color={theme['blue']} weight="fill" />
+                    <label>tempo</label>
+                </div>
+                <div>
+                    <ChatCircle size={20} color={theme['blue']} weight="fill" />
+                    <label>comentarios</label>
+                </div>
+            </ExtraInformations>
+            </BoxInfoPost>
+
+            <ContentPost>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium modi itaque temporibus! Cumque amet, saepe perspiciatis placeat a nihil quo nostrum ab mollitia dolorum molestiae, veniam ex quod, totam blanditiis?</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium modi itaque temporibus! Cumque amet, saepe perspiciatis placeat a nihil quo nostrum ab mollitia dolorum molestiae, veniam ex quod, totam blanditiis?</p>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium modi itaque temporibus! Cumque amet, saepe perspiciatis placeat a nihil quo nostrum ab mollitia dolorum molestiae, veniam ex quod, totam blanditiis?</p>
+            </ContentPost>
+        </ContainerPost>
+        
     )
 }
---
+++ src/pages/Post/styles.ts
@@ -0,0 +1,95 @@
+import styled from "styled-components";
+
+export const ContainerPost = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    max-width: 864px;
+    margin: 0 auto 0; 
+
+    gap: 2rem;
+
+    align-items: center;
+    justify-content: center;
+
+    margin: 0 auto 0; //top Right Bottom Left
+    padding: 0 1.5rem; // espaçamento nas laterias do conteudo
+
+`
+
+export const BoxInfoPost = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 2rem;
+    
+    margin-top: -95px;
+    
+    background: ${(props) => props.theme['base-profile']};
+    border: none;
+    border-radius: 10px;
+    font-family: 'Nunito', sans-serif;
+`
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+
+    button {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+        gap: 0.5rem;
+
+        background: transparent;
+        border: none;
+
+        color: ${(props) => props.theme['blue']};
+        cursor: pointer;
+        svg {
+            line-height: 0;
+        }
+    }
+`
+
+export const ExtraInformations = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: start;
+
+    margin-top: 1rem;
+    gap: 2rem;
+
+    div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: center;
+
+        color: ${(props) => props.theme['base-label']};
+        font-family: 'Nunito', sans-serif;
+        gap: 0.5rem;
+    }
+
+`
+
+export const ContentPost = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+
+    margin-top: 1rem;
+    gap: 1rem;
+
+    p {
+        color: ${(props) => props.theme['base-text']};
+        font-size: 1rem;
+        font-family: 'Nunito', sans-serif;
+
+    }
+
+`
